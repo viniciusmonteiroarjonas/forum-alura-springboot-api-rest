7f1349d09ed9b15b85469b7789a8386d4c730c21
vinicius monteiro <vma.developer@gmail.com>
2021-05-06 20:25:12 -0300

    create function create topicos

--- src/main/java/br/com/alura/forum/modelo/Topico.java
+++ src/main/java/br/com/alura/forum/modelo/Topico.java
@@ -29,6 +29,17 @@ public class Topico {
 	private Curso curso;
 	@OneToMany(mappedBy = "topico")
 	private List<Resposta> respostas = new ArrayList<Resposta>();
+	
+	public Topico() {}
+	
+	
+	public Topico(String titulo, String mensagem, Curso curso) {
+		this.titulo = titulo;
+		this.mensagem = mensagem;
+		this.curso = curso;
+	}
+
+
 
 
 	@Override
