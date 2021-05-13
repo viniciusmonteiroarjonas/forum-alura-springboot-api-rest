de6d568c3650572bc481bb74f6d1f608a1468fbe
vinicius monteiro <vma.developer@gmail.com>
2021-05-13 15:45:42 -0300

    configuration token jwt authenticated in app

---
+++ src/main/java/br/com/alura/forum/config/security/TokenService.java
@@ -0,0 +1,39 @@
+package br.com.alura.forum.config.security;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import br.com.alura.forum.modelo.Usuario;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Service
+public class TokenService {
+	
+	@Value("${forum.jwt.expiration}")
+	private String expiration;
+	
+	@Value("${forum.jwt.secret}")
+	private String secret;
+
+	public String gerarToken(Authentication authentication) {
+		Usuario logado = (Usuario) authentication.getPrincipal(); 
+		Date dtCriacaoToken = new Date();
+		Date dtexpiration = new Date(dtCriacaoToken.getTime() + Long.parseLong(expiration));
+		System.out.println("vinicius");
+
+		System.out.println(logado);
+		
+		return Jwts.builder()
+				.setIssuer("Api Forum alura")
+				.setSubject(logado.getId().toString())
+				.setIssuedAt(dtCriacaoToken)
+				.setExpiration(dtexpiration)
+				.signWith(SignatureAlgorithm.HS256, secret)
+				.compact();
+	}
+
+}
